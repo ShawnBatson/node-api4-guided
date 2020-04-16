684193497daca78f497eed260bcdd61af8f88261
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-04-15 20:30:31 -0500

    added

--- shouts/shouts-router.js
+++ shouts/shouts-router.js
@@ -1,45 +1,57 @@
-const express = require("express")
-const shoutsModel = require("./shouts-model")
+const express = require("express");
+const shoutsModel = require("./shouts-model");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	shoutsModel.find()
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
-})
+router.get("/", async (req, res, next) => {
+    try {
+        const data = await shoutsModel.find();
+        res.status(200).json(data);
+    } catch (err) {
+        next(err);
+    }
+    // https://javascript.info/async-await
+    // .then((data) => res.status(200).json(data))
+    // .catch((err) => next(err))
+});
 
 router.get("/:id", validateShoutId(), (req, res, next) => {
-	res.status(200).json(req.shout)
-})
+    res.status(200).json(req.shout);
+});
 
 router.post("/", (req, res, next) => {
-	shoutsModel.add(req.body)
-		.then((data) => res.status(201).json(data))
-		.catch((err) => next(err))
-})
+    shoutsModel
+        .add(req.body)
+        .then((data) => res.status(201).json(data))
+        .catch((err) => next(err));
+});
 
-router.delete("/:id", validateShoutId(), (req, res, next) => {
-	Shouts.remove(req.params.id)
-		.then(() => res.status(204).end())
-		.catch((err) => next(err))
-})
+router.delete("/:id", validateShoutId(), async (req, res, next) => {
+    try {
+        await Shouts.remove(req.params.id);
+        res.status(204).end();
+    } catch (err) {
+        next(err);
+    }
+});
 
 function validateShoutId() {
-	return (req, res, next) => {
-		shoutsModel.findById(req.params.id)
-			.then((shout) => {
-				if (shout) {
-					req.shout = shout
-					next()
-				} else {
-					res.status(404).json({
-						message: "Could not find shout",
-					})
-				}
-			})
-			.catch(next)
-	}
+    return async (req, res, next) => {
+        try {
+            const shout = await shoutsModel;
+            await findById(req.params.id);
+            if (shout) {
+                req.shout = shout;
+                next();
+            } else {
+                res.status(404).json({
+                    message: "Could not find shout",
+                });
+            }
+        } catch (err) {
+            next(err);
+        }
+    };
 }
 
-module.exports = router
+module.exports = router;
